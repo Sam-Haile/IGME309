ee0616aa5d98fa3a2df1aa6e4299fc8245005352
Sam-Haile <92647194+Sam-Haile@users.noreply.github.com>
2023-06-30 19:59:56 -0400

    E08
    E08 Completed

--- E08 - Projections/AppClass.cpp
+++ E08 - Projections/AppClass.cpp
@@ -1,8 +1,7 @@
 #include "AppClass.h"
 void Application::InitVariables(void)
 {
-	////Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Sam Haile - sdh5898@rit.edu";
 	vector3 v3Position(0.0f, 0.0f, 10.0f);
 	vector3 v3Target = ZERO_V3;
 	vector3 v3Upward = AXIS_Y;
@@ -59,21 +58,30 @@ void Application::Display(void)
 		break;
 	case 2:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPerspective(false);
 		break;
 	case 3:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetUp(AXIS_Z * -1.0f);
+		m_pCamera->SetPosition(AXIS_X * 20.0f);
 		break;
 	case 4:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPosition(AXIS_Z * -15.0f);
 		break;
 	case 5:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPosition(AXIS_Z * -10.0f);
+		m_pCamera->SetFOV(14);
 		break;
 	case 6:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPosition(AXIS_Z * -15.0f);
+		m_pCamera->SetNearFar(vector2(1.0f, 10.0f));
 		break;
 	case 7:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetUp(AXIS_Y * -1.0f);
 		break;
 	}
 
